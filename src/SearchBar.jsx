cbf65d0e141ad02460c62172c9cd84c69021e0c5
Hassan Razak <hassanrazak1991@gmail.com>
2023-02-25 17:20:27 -0500

    search bar hooked up to state and submit action.

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,33 +1,34 @@
-
+import {useState,useEffect} from 'react'
 
 const SearchBar = () => {
+
+    const[search,setSearch] = useState('')
+
+   const handleSearch = (e) =>{
+    e.preventDefault();
+    console.log('clicked')
+   }
 
 
     return (    
         <>
-   <div className="flex flex-wrap -mx-4 -mb-4 md:mb-0">
-  <div className="w-full md:w-2/3 px-4 mb-4 md:mb-0"><div className="flex flex-wrap -mx-4 -mb-4 md:mb-0">
-    <div className="w-full md:w-2/3 px-4 mb-4 md:mb-0"><div className="mb-4">
+  <form onSubmit={handleSearch} action="#" method="post" className=""><div className="mb-4">
 
-      <input className="block w-full p-4 font-heading text-gray-300 placeholder-gray-300 bg-gray-50 rounded outline-none" type="text" name="field-name" placeholder="SEARCH......." id=""/>
-      </div></div>
-    <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0"><a className="inline-block py-4 px-6 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 rounded-sm transition duration-200" href="#">ENTER</a></div>
-    </div></div>
-  <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0"><div className="mb-6">
-    <div className="mb-1">
-      <label>
-        <input type="radio" name="radio-name" value="option 1" checked=""/>
-        <span className="ml-1">NBC</span>
-      </label>
-    </div>
-    <div>
-      <label>
-        <input type="radio" name="radio-name" value="option 2"/>
-        <span className="ml-1">BBC</span>
-      </label>
-    </div>
-    </div></div>
+<input className="block w-full p-4 font-heading text-gray-300 placeholder-gray-300 bg-gray-50 rounded outline-none" value={search} onChange={(e) =>setSearch(e.target.value)} type="text" name="field-name" placeholder="SEARCH....." id=""/>
+</div><div className="mb-6">
+<div className="mb-1">
+  <label>
+    <input type="radio" name="radio-name" value="option 1" checked=""/>
+    <span className="ml-1">Radio option 1</span>
+  </label>
 </div>
+<div>
+  <label>
+    <input type="radio" name="radio-name" value="option 2"/>
+    <span className="ml-1">CNN</span>
+  </label>
+</div>
+</div><button className="inline-block py-4 px-6 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 rounded-sm transition duration-200" type="submit">ENTER</button></form>
         </>
      );
 }
